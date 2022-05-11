716ea3e299eb60efa4e81d2d864666a666b00b9f
Pete Hamrick <pete.hamrick@gmail.com>
2022-05-11 09:53:59 -0700

    added comment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
 export default function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Switch>
           <Route>
             {/* this home page route should list all the beanies */}
+            {/* adding new comment for committing practice */}
           </Route>
-          <Route> 
+          <Route>
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
           </Route>
         </Switch>
